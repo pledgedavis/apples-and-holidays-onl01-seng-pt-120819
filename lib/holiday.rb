0b057fa0cf593b2fe4f925d60ad80813be75eb0f
pledgedavis <pledgedavis14@gmail.com>
2019-12-22 05:51:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash[:fourth_of_july][:memorial_day]
+  holiday_hash[:fourth_of_july][memorial_day]
 
 end
 
