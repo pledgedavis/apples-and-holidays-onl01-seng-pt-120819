f46b5269c57bc8815ed13f4bcc4d35b847c33ce4
pledgedavis <pledgedavis14@gmail.com>
2019-12-22 06:01:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
 puts "#{season.to-s.capitalize}:"
 holiday.each do |event, supplies|
-  puts "#"
+  puts "#{event"
 end
 end
 
