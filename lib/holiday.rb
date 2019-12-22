392a1dc660918b24ad65372c3ea3c94158483d80
pledgedavis <pledgedavis14@gmail.com>
2019-12-22 05:47:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-        holiday_hash[:winter].map do |key,va
+        holiday_hash[:winter].map do |key,value|
+          
 # .collect { |k, v| v }
 end
 
