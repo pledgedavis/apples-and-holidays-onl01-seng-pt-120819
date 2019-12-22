27c879936563061d87db3974511a2747e8f643c6
pledgedavis <pledgedavis14@gmail.com>
2019-12-22 04:44:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
- holiday_hash[:thanksgiving][:columbus_day] = supply
+ holiday_hash[:thanksgiving][:columbus_day] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
