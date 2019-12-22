6ce22db11c5d319cf99dc889b3e0b22dc65025ad
pledgedavis <pledgedavis14@gmail.com>
2019-12-22 05:55:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-puts "#{season.to-s.capitalize"
+puts "#{season.to-s.capitalize}:"
 end
 
 def all_holidays_with_bbq(holiday_hash)
